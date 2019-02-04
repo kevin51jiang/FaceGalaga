6ce92d4cd7b9febb03e81a39eaaf7176942cd963
Kevin Jiang <kevin51jiang@email.com>
2019-02-03 20:49:20 -0500

    Test confirmed to work, not need to work on clickableness

--- out/source/ProcessingIsBad.java
+++ out/source/ProcessingIsBad.java
@@ -1,14 +1,32 @@
-import java.util.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class ProcessingIsBad extends PApplet {
+
+
 
 ScreenManager sm;
-void setup() {
-    size(640, 480);
-      sm = new ScreenManager();
-  sm.add(new TestMenu(sm));
+public void setup() {
+    
+      
+  sm.add(new TestMenu());
 }
 
 
-void draw() {
+public void draw() {
     sm.display();
 
 }
@@ -24,25 +42,18 @@ void draw() {
 *
 */
 abstract class Screen {
-    private ScreenManager scrnMgr;
+
     public String uid;
     public abstract void display();
 
-    public Screen (ScreenManager scrnMgr) {
-        this.scrnMgr = scrnMgr;
+    public Screen () {
+
     }
 
-    public void setManager(ScreenManager scrnMgr) {
-        this.scrnMgr = scrnMgr;
-    }
-
-    public ScreenManager getScreenManager(){
-        return this.scrnMgr;
-    }
 }
 
 
-class ScreenManager {
+static class ScreenManager {
     public HashMap<String, Screen> screens = new HashMap<String, Screen>();
     public String currentScreenUid;
 
@@ -97,4 +108,14 @@ class TestMenu extends Screen{
         
     }
 
+}
+  public void settings() {  size(640, 480); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "ProcessingIsBad" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
